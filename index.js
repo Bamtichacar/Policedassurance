06802fe992e6f02d2c457960b21d3276b459da2c
Lauliek <laureline.pellegrini@gmail.com>
2024-10-14 14:49:55 +0200

    // EXERCICE PRIX D'UNE PHOTOCOPIE

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+
+
+//EXERCICE POLICE D'ASSURANCE
+
+
 // Familles des tarifs, le A est le - cher
-const tarifA = "A";
+/* const tarifA = "A";
 const tarifB = "B";
 const tarifC = "C";
 const tarifD = "D";
@@ -9,7 +14,7 @@ let ageConducteur = document.getElementById("age");
 let nbAnneesAnciennetePermis = 5;
 let accidentResponsable = 0;
 let ancienneteAssurance = 1;
-
+ */
 /* à voir si rajout en l'écrivant comme il faut
 let clientassurance = "oui"
 if(clientassurance===oui) {
@@ -18,7 +23,7 @@ if(clientassurance===oui) {
 ancienneteAssurance=0
  */
 
-function determinationdutarifinitial() {
+/* function determinationdutarifinitial() {
     if((ageConducteur<25 && nbAnneesAnciennetePermis<2 && accidentResponsable===0)) {
         if(ancienneteAssurance>=1) {
             return tarifC;
@@ -63,10 +68,9 @@ function determinationdutarifinitial() {
 
 console.log(determinationdutarifinitial());
 
-
-
-
-/* const input = prompt('What\'s your name?');
+ */
+/* A VOIR
+const input = prompt('What\'s your name?');
 alert(`Your name is ${input}`);
 console.log(input);
 
@@ -79,3 +83,23 @@ console.log(input2);
 let ageConducteur = Window.prompt('Quel est votre âge');
 
  */
+
+
+
+// EXERCICE PRIX D'UNE PHOTOCOPIE
+
+
+function prixDesPhotocopies(n) {
+    let prix = 0;  
+    if(n <= 10) {
+        prix = n * 0.10;
+    } else if(n > 10 && n <= 30) {
+        prix = 10*0.10 + (n-10)*0.09;
+    } else if(n>30) {
+        prix = 10*0.10 + 20*0.09 + (n-30)*0.08;
+    }
+    return prix;
+}
+
+console.log(`${prixDesPhotocopies(30)} €`);
+
